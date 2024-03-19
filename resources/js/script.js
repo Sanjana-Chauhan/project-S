6de2a9da0c6c4713cdb843f4ffa55c9460a5f378
Yash Duhan <70028651+YashDuhan@users.noreply.github.com>
2024-03-20 02:08:57 +0530

    More Minor Changes

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,10 +1,26 @@
-let navLinks = document.querySelectorAll('header ul li a');
+/*Clear highlight*/
+let navLinks = document.querySelectorAll("header ul li a");
 
-  navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-      navLinks.forEach(otherLink => {
-        otherLink.classList.remove('highlight');
-      });
-      link.classList.add('highlight');
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    navLinks.forEach((otherLink) => {
+      otherLink.classList.remove("highlight");
     });
+    link.classList.add("highlight");
   });
+});
+
+//Preloader
+document.addEventListener("DOMContentLoaded", function () {
+  var counter = document.getElementById("counter");
+  var percent = 0;
+  var interval = setInterval(function () {
+    percent++;
+    counter.textContent = percent + "%";
+    if (percent >= 100) {
+      clearInterval(interval);
+      // Once loading is complete, hide the loader
+      document.getElementById("loader").style.display = "none";
+    }
+  }, 20); // Adjust the interval as needed
+});
